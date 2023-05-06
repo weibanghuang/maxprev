bfab195c3ec2659c703c606cf16ebb6deb5e495e
weibanghuang <77127321+weibanghuang@users.noreply.github.com>
2023-05-06 04:56:55 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -179,9 +179,9 @@ function toggle_data(){
       <div class="button_wrap">
         <input type="file" name="uploadfile" id="fileInput" accept=".csv" class = "input_button"/>
         <label for="fileInput" class="import_button">Import Data</label>
-        <div class="export_button">Export Data</div>
-        <div class="stats_button">View Max Prev</div>
-        <div class="danger_button">Delete</div>
+        <button class="export_button">Export Data</button>
+        <button class="stats_button">View Max Prev</button>
+        <button class="danger_button">Delete</button>
       </div>
     `;
     document.querySelector('.pop_up').innerHTML = popup;
